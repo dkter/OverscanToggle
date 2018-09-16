1c0fe4ecfeae85c63de1be073cc28f32e0801ccf
David Teresi <dkteresi@gmail.com>
2018-09-16 11:29:18 -0400

    More licensing

--- OverscanToggle/MainActivity.cs
+++ OverscanToggle/MainActivity.cs
@@ -1,4 +1,7 @@
-﻿using Android.App;
+﻿/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+using Android.App;
 using Android.Widget;
 using Android.OS;
 using Android.Service.QuickSettings;
